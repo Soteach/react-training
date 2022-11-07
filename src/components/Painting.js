374f82d73280699613988f254131dcd9973d720d
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-11-08 00:04:19 +0200

    add app&painting

---
+++ src/components/Painting.js
@@ -0,0 +1,22 @@
+import defaultImage from '../components/default-img.jpg';
+export default function Painting({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не відомо',
+  price,
+}) {
+  return (
+    <div>
+      <img src={imageUrl} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор:
+        <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Ціна: {price} кредитів</p>
+      <p>Доступність:закінчується, або є в наявності</p>
+      <button type="button">Додати в кошик</button>
+    </div>
+  );
+}
