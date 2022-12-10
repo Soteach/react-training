9d4ceb90dc915796520f191a93ffc3df26464e41
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-12-10 19:09:11 +0200

    add Modal window

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,46 @@
+import React, { Component } from 'react';
 import Clock from './Clock/Clock';
+import Container from './Container/Container';
+import { Modal } from './Modal/Modal';
 import './App.css';
 
-export default function App() {
-  return <Clock />;
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <Container>
+        <button type="button" onClick={this.toggleModal}>
+          Open
+        </button>
+        {showModal && (
+          <Modal>
+            <h1>This is modal content as children</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              maiores blanditiis vitae animi excepturi doloribus cupiditate
+              ipsam voluptatibus debitis odit, itaque reiciendis in assumenda
+              reprehenderit accusamus dolor omnis! Odit maxime molestias dolores
+              atque eaque. Architecto officia quo iste nostrum distinctio
+              accusantium magnam perspiciatis dolores, alias quisquam ducimus
+              neque voluptatum? Dignissimos?
+            </p>
+          </Modal>
+        )}
+
+        {/* {showModal && <Clock />} */}
+      </Container>
+    );
+  }
 }
+
+export default App;
